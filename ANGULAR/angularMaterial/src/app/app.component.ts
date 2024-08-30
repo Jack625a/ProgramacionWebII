0a270af97f6b100b5a9e259f17f4bee91d4dc678
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-08-29 21:48:40 -0400

    Angular Material

---
+++ ANGULAR/angularMaterial/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+//Paso 1. importar el modulo de Botones
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, MatIconModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angularMaterial';
+}
